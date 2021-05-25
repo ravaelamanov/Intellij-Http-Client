1d4b5e7732e08ebf3239940f1dbc4650865660d3
AbduGaffar <abdugaffarkutbiddinov@gmail.com>
2021-05-26 00:39:19 +0300

    creating body pane text area and functionality implemented

--- src/main/kotlin/ui/toolWindow/request/RequestPane.kt
+++ src/main/kotlin/ui/toolWindow/request/RequestPane.kt
@@ -25,6 +25,7 @@ class RequestPane : JComponent() {
     private var requestTabbedPane: JTabbedPane = JTabbedPane()
     private val paramsRequestPane = ParamsRequestPane().createPanel()
     private val headersRequestPane = HeadersRequestPane().createPanel()
+    private val bodyRequestPane = BodyRequestPane().createPanel()
     private val tabbedPanes = TabbedPaneList()
     val urlTextField = JTextField("google", COLUMNS_NUMBER)
     private val savedSelection = RequestPanePersistenceService.instance.objState.method
@@ -65,8 +66,7 @@ class RequestPane : JComponent() {
         textFieldUpdate()
         requestTabbedPane.addTab(tabbedPanes.listOfPanes[0], paramsRequestPane)
         requestTabbedPane.addTab(tabbedPanes.listOfPanes[1], headersRequestPane)
-        requestTabbedPane.addTab(tabbedPanes.listOfPanes[2], JPanel())
-        requestTabbedPane.addTab(tabbedPanes.listOfPanes[3], JPanel())
+        requestTabbedPane.addTab(tabbedPanes.listOfPanes[2], bodyRequestPane)
         row {
             cell {
                 methodsComboBox()
