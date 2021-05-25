f0ae4bf25e9fc9c7fc5b4573ba5cf853df1136ec
ravaelamanov <ravaelamanov@gmail.com>
2021-05-25 22:42:42 +0300

    Service now builds the request with values that are going to be persisted from UI

--- src/main/kotlin/services/impl/HttpRequestSenderServiceImpl.kt
+++ src/main/kotlin/services/impl/HttpRequestSenderServiceImpl.kt
@@ -1,11 +1,16 @@
 package services.impl
 
 import services.HttpRequestSenderService
+import services.extensions.addParameter
+import services.persistence.RequestPanePersistenceService
+import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 class HttpRequestSenderServiceImpl : HttpRequestSenderService<String> {
+
+    private val requestPaneState = RequestPanePersistenceService.instance.objState
 
     override fun send(): HttpResponse<String> {
         val httpClient = HttpClient.newBuilder().build()
@@ -16,7 +21,20 @@ class HttpRequestSenderServiceImpl : HttpRequestSenderService<String> {
     }
 
     private fun buildRequestFromUI(): HttpRequest {
-        TODO()
+        var uri = URI(requestPaneState.url.trim())
+        requestPaneState.parametersKeyValueTable.forEach { uri = uri.addParameter(it[0] ?: "", it[1] ?: "") }
+        val filteredHeaders = requestPaneState.headersKeyValueTable.filterNot { it[0] == null || it [0] == "" }
+        val requestBuilder = HttpRequest.newBuilder()
+        if (filteredHeaders.isNotEmpty()) {
+            requestBuilder.headers(*filteredHeaders.flatMap { it.toList() }.toTypedArray())
+        }
+        return requestBuilder
+            .method(
+                requestPaneState.method.toUpperCase(),
+                HttpRequest.BodyPublishers.ofString(requestPaneState.body)
+            )
+            .uri(uri)
+            .build()
     }
 
     private fun persistResponse(httpResponse: HttpResponse<String>) {
