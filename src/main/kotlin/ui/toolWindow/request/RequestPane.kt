79366fa3e7fd78ab30114ef78d8aa1f4d3beb4e1
AbduGaffar <abdugaffarkutbiddinov@gmail.com>
2021-05-25 23:29:46 +0300

    textfield and send fields implement

--- src/main/kotlin/ui/toolWindow/request/RequestPane.kt
+++ src/main/kotlin/ui/toolWindow/request/RequestPane.kt
@@ -4,19 +4,31 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.layout.panel
+import services.HttpRequestSenderService
+import services.persistence.RequestPanePersistenceService
 import ui.toolWindow.TabbedPaneList
 import javax.swing.JComboBox
 import javax.swing.JComponent
+import javax.swing.JTextField
 
 
-class RequestPane: JComponent() {
+class RequestPane : JComponent() {
+    companion object {
+        private const val COLUMNS_NUMBER: Int = 20
+    }
+
     private val tabbedPanes = TabbedPaneList()
     val comboBoxModel = CollectionComboBoxModel(tabbedPanes.methodsList)
     val methodsComboBox = JComboBox(comboBoxModel)
+    val urlTextField = JTextField("google", COLUMNS_NUMBER)
     fun createRequestPane(): DialogPanel = panel(title = "REQUEST") {
         row {
             cell {
                 methodsComboBox()
+                urlTextField()
+                button("SEND") {
+
+                }
             }
         }
     }
