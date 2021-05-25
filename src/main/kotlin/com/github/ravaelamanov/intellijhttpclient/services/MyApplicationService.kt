86a73c0ae4e88864cef3a7afd01f620db2ecb70b
GitHub Action <action@github.com>
2021-05-25 17:02:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ravaelamanov/intellijhttpclient/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ravaelamanov.intellijhttpclient.services
+
+import com.github.ravaelamanov.intellijhttpclient.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
