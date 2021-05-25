9ba1f888e0aeb6c725e941281177794e5f176142
ravaelamanov <ravaelamanov@gmail.com>
2021-05-25 22:06:14 +0300

    Implementation of HttpRequestSenderService that sends HTTP requests using HttpClient

---
+++ src/main/kotlin/services/impl/HttpRequestSenderServiceImpl.kt
@@ -0,0 +1,26 @@
+package services.impl
+
+import services.HttpRequestSenderService
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+
+class HttpRequestSenderServiceImpl : HttpRequestSenderService<String> {
+
+    override fun send(): HttpResponse<String> {
+        val httpClient = HttpClient.newBuilder().build()
+        val httpRequest = buildRequestFromUI()
+        val httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString())
+        persistResponse(httpResponse)
+        return httpResponse
+    }
+
+    private fun buildRequestFromUI(): HttpRequest {
+        TODO()
+    }
+
+    private fun persistResponse(httpResponse: HttpResponse<String>) {
+        TODO()
+    }
+}
