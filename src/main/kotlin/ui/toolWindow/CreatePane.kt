cf58174f3a0dab01ade834d381af7ad5aff7bae1
AbduGaffar <abdugaffarkutbiddinov@gmail.com>
2021-05-25 23:02:53 +0300

    created request empty pane

--- src/main/kotlin/ui/toolWindow/CreatePane.kt
+++ src/main/kotlin/ui/toolWindow/CreatePane.kt
@@ -1,12 +1,14 @@
 package ui.toolWindow
 
+import ui.toolWindow.request.RequestPane
 import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.JSplitPane
 
 class CreatePane : JComponent() {
+    var requestPane = RequestPane()
     var splitPane = JSplitPane(
-        JSplitPane.VERTICAL_SPLIT, JPanel(), JPanel()
+        JSplitPane.VERTICAL_SPLIT,  requestPane.createRequestPane(), JPanel()
     )
     init {
         splitPane.resizeWeight = DIVIDE_PROPORTION
