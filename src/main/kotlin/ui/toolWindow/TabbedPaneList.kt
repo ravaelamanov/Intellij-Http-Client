55cc354d7271e19c8e8ab5e5b9405b1c5ef667b4
AbduGaffar <abdugaffarkutbiddinov@gmail.com>
2021-05-25 23:21:01 +0300

    dropdownbox implement

---
+++ src/main/kotlin/ui/toolWindow/TabbedPaneList.kt
@@ -0,0 +1,7 @@
+package ui.toolWindow
+
+class TabbedPaneList {
+    val listOfPanes: List<String> = arrayListOf("Params", "Headers", "Body", "Auth")
+    val methodsList: List<String> = arrayListOf("GET", "HEAD", "POST", "PUT",
+        "DELETE", "OPTIONS", "TRACE", "PATCH")
+}
--- src/main/kotlin/ui/toolWindow/request/RequestPane.kt
+++ src/main/kotlin/ui/toolWindow/request/RequestPane.kt
@@ -1,4 +1,23 @@
 package ui.toolWindow.request
 
-class RequestPane {
+import com.intellij.openapi.components.service
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.CollectionComboBoxModel
+import com.intellij.ui.layout.panel
+import ui.toolWindow.TabbedPaneList
+import javax.swing.JComboBox
+import javax.swing.JComponent
+
+
+class RequestPane: JComponent() {
+    private val tabbedPanes = TabbedPaneList()
+    val comboBoxModel = CollectionComboBoxModel(tabbedPanes.methodsList)
+    val methodsComboBox = JComboBox(comboBoxModel)
+    fun createRequestPane(): DialogPanel = panel(title = "REQUEST") {
+        row {
+            cell {
+                methodsComboBox()
+            }
+        }
+    }
 }
