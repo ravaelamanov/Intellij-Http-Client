42c8fae8c764bb3d6653af76d28b2615ef08f808
ravaelamanov <ravaelamanov@gmail.com>
2021-05-25 23:16:28 +0300

    Service to persist received http response data

---
+++ src/main/kotlin/services/persistence/ResponsePanePersistenceService.kt
@@ -0,0 +1,26 @@
+package services.persistence
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+import com.intellij.util.xmlb.annotations.OptionTag
+import services.persistence.converters.VectorConverter
+import java.util.Vector
+
+@Service
+@State(name = "ResponsePanePersistenceService", storages = [Storage("responsePanePersistenceService.xml")])
+class ResponsePanePersistenceService : AbstractPersistenceService<ResponsePanePersistenceService.State>() {
+    companion object {
+        val instance: ResponsePanePersistenceService
+            get() = service()
+    }
+
+    class State {
+        var body: String = ""
+        @OptionTag(converter = VectorConverter::class)
+        var headersKeyValueTable: Vector<Vector<String>> = Vector(1)
+    }
+
+    override var objState = State()
+}
