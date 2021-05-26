671298542666b9cece8f5c096fb7bfb5e92115a1
ravaelamanov <ravaelamanov@gmail.com>
2021-05-26 23:02:20 +0300

    Created a enum to be used by UI to display auth strategies in a combo box

--- src/main/kotlin/services/auth/AuthenticationProvider.kt
+++ src/main/kotlin/services/auth/AuthenticationProvider.kt
@@ -1,7 +1,12 @@
 package services.auth
 
+import services.auth.impl.BasicAuthenticationProvider
 import java.net.http.HttpRequest
 
 interface AuthenticationProvider {
     fun authenticate(httpRequest: HttpRequest): HttpRequest
+
+    enum class Strategies(val provider: AuthenticationProvider) {
+        Basic(BasicAuthenticationProvider());
+    }
 }
