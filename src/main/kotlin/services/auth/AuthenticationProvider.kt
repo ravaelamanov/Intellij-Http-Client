12d0d11f0d946b1dcbd0ce788a4e5ebbd947fd07
ravaelamanov <ravaelamanov@gmail.com>
2021-05-26 23:14:34 +0300

    Added NoAuth provider to strategies enum

--- src/main/kotlin/services/auth/AuthenticationProvider.kt
+++ src/main/kotlin/services/auth/AuthenticationProvider.kt
@@ -1,12 +1,18 @@
 package services.auth
 
 import services.auth.impl.BasicAuthenticationProvider
+import services.auth.impl.NoAuthenticationProvider
 import java.net.http.HttpRequest
 
 interface AuthenticationProvider {
     fun authenticate(httpRequest: HttpRequest): HttpRequest
 
     enum class Strategies(val provider: AuthenticationProvider) {
+        No_Auth(NoAuthenticationProvider()),
         Basic(BasicAuthenticationProvider());
+
+        override fun toString(): String {
+            return super.toString().replace('_', ' ')
+        }
     }
 }
