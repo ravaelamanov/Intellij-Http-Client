e0cf58546ab0e621772e34f516d675486a2748f0
ravaelamanov <ravaelamanov@gmail.com>
2021-05-27 20:32:26 +0300

    Refactoring text data from String into Document

--- src/main/kotlin/services/persistence/RequestPanePersistenceService.kt
+++ src/main/kotlin/services/persistence/RequestPanePersistenceService.kt
@@ -6,8 +6,10 @@ import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 import com.intellij.util.xmlb.annotations.OptionTag
 import services.auth.AuthenticationProvider
+import services.persistence.converters.DocumentConverter
 import services.persistence.converters.VectorConverter
 import java.util.Vector
+import javax.swing.text.Document
 
 @Service
 @State(name = "RequestPanePersistenceService", storages = [Storage("requestPanePersistenceService.xml")])
@@ -18,13 +20,20 @@ class RequestPanePersistenceService : AbstractPersistenceService<RequestPanePers
     }
 
     class State {
-        var method: String = "GET"
-        var url: String = ""
+        @OptionTag(converter = DocumentConverter::class)
+        var method: Document = plainDocumentOfGapContent("GET")
+
+        @OptionTag(converter = DocumentConverter::class)
+        var url: Document = plainDocumentOfGapContent("")
+
         @OptionTag(converter = VectorConverter::class)
         var parametersKeyValueTable: Vector<Vector<String>> = Vector(1)
+
         @OptionTag(converter = VectorConverter::class)
         var headersKeyValueTable: Vector<Vector<String>> = Vector(1)
-        var body: String = ""
+
+        @OptionTag(converter = DocumentConverter::class)
+        var body: Document = plainDocumentOfGapContent("")
         var auth = AuthenticationProvider.Strategies.No_Auth
 
         init {
