3f3f3db8bbbaf4cc345216e256783b77390e7c36
ravaelamanov <ravaelamanov@gmail.com>
2021-05-28 12:47:44 +0300

    Using extension method now to get text of a Document object

--- src/main/kotlin/services/persistence/converters/DocumentConverter.kt
+++ src/main/kotlin/services/persistence/converters/DocumentConverter.kt
@@ -1,12 +1,13 @@
 package services.persistence.converters
 
 import com.intellij.util.xmlb.Converter
+import services.extensions.allText
 import services.persistence.plainDocumentOfGapContent
 import javax.swing.text.Document
 
 class DocumentConverter : Converter<Document>() {
-    override fun toString(value: Document): String? {
-        return value.getText(0, value.length)
+    override fun toString(value: Document): String {
+        return value.allText()
     }
 
     override fun fromString(value: String): Document {
